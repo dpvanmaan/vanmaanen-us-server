0d1cb900965025da6dc0647275cb12717b5f12ce
David vanMaanen <david@vanmaanen.us>
2015-06-08 11:35:30 -0400

    Filter backend added for parameter support for misc models

--- misc/views.py
+++ misc/views.py
@@ -1,15 +1,20 @@
 from django.shortcuts import render
 from rest_framework.permissions import IsAuthenticatedOrReadOnly, IsAuthenticated
 from rest_framework import viewsets
+from rest_framework import filters
 from .models import Image, Text, Category
 from .serializers import ImageSerializer, TextSerializer, CategorySerializer
 # Create your views here.
 
 class ImageViewSet(viewsets.ModelViewSet):
+    filter_backends = (filters.DjangoFilterBackend,)
+    filter_fields = ('category__name','name')
     queryset=Image.objects.all()
     serializer_class=ImageSerializer
     permission_classes=[IsAuthenticatedOrReadOnly]
 class TextViewSet(viewsets.ModelViewSet):
+    filter_backends = (filters.DjangoFilterBackend,)
+    filter_fields = ('category__name','name')
     queryset=Text.objects.all()
     serializer_class=TextSerializer
     permission_classes=[IsAuthenticatedOrReadOnly]
